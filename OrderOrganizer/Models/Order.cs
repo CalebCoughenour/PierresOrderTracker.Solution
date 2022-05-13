4521b1024ad8fa8bf2d6b4221641339842223f8d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-13 11:13:48 -0700

    add method to retrieve all orders in order list

--- OrderOrganizer/Models/Order.cs
+++ OrderOrganizer/Models/Order.cs
@@ -7,24 +7,24 @@ namespace OrderOrganizer.Models
     public string OrderName { get; set; }
     public string OrderDescription { get; set; }
     public string OrderDate { get; set; }
-    private static double _Price = 5;
     private static List<Order> _orders = new List<Order>{ };
 
-    public static double Price
-    {
-      get { return _Price; }
-      set { _Price = value; }
-    }
+
     public Order(string orderName, string orderDescription, string orderDate)
     {
       OrderName = orderName;
       OrderDescription = orderDescription;
       OrderDate = orderDate;
-      _Price = Price;
+      _orders.Add(this);
     }
     public static void ClearAll()
     {
       _orders.Clear();
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _orders;
     }
   }
 }
