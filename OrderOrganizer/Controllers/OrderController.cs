c10799f389d44f75c50af01a42b8007b7aac4857
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-13 15:45:56 -0700

    add path to create orders

--- OrderOrganizer/Controllers/OrderController.cs
+++ OrderOrganizer/Controllers/OrderController.cs
@@ -7,11 +7,29 @@ namespace OrdersOrganizer.Controllers
 {
   public class OrdersController : Controller
   {
+    [HttpGet("/orders")]
+    public ActionResult Show()
+    {
+      List<Order> orders = Order.GetAll();
+      return View(orders);
+    }
+    
     [HttpGet("/vendors/{vendorName}/orders/new")]
     public ActionResult New(string vendorName)
     {
       Vendor vendor = Vendor.FindVendor(vendorName);
-      return View(vendor);
+      return View("New", vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("orders", order);
+      model.Add("vendors", vendor);
+      return View(model);
     }
   }
 }
