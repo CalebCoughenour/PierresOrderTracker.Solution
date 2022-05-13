ece2064ed6e09892685addba7dc55eaca5e52482
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-13 09:49:16 -0700

    add disposable method to tests

--- OrderOrganizer.Tests/ModelsTests/VendorTests.cs
+++ OrderOrganizer.Tests/ModelsTests/VendorTests.cs
@@ -6,8 +6,13 @@ using OrderOrganizer.Models;
 namespace OrderOrganizer.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void VendorConstructor_CreateInstanceOfVendor_Vendor()
     {
