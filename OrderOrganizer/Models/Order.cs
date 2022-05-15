806fb51403c446026b5b5eccaa8ed3bd7eb179ee
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-15 14:44:23 -0700

    add price adjuster for order type

--- OrderOrganizer/Models/Order.cs
+++ OrderOrganizer/Models/Order.cs
@@ -8,18 +8,39 @@ namespace OrderOrganizer.Models
     public string OrderDescription { get; set; }
     public string OrderDate { get; set; }
     public int Id { get; }
-    public int Price { get; }
+    public int Price { get; set; }
+    public string OrderType { get; set; }
+    public bool PriceAdjusted { get; set; }
     private static List<Order> _orders = new List<Order>{ };
 
 
-    public Order(string orderName, string orderDescription, string orderDate)
+    public Order(string orderName, string orderDescription, string orderDate, string orderType)
     {
       OrderName = orderName;
       OrderDescription = orderDescription;
       OrderDate = orderDate;
+      OrderType = orderType;
       _orders.Add(this);
       Id = _orders.Count;
       Price = 5;
+      PriceAdjusted = false;
+    }
+
+    public static void PriceAdjuster(string orderType) 
+    {
+      foreach (Order order in _orders)
+      {
+        if(orderType == "bread" && order.PriceAdjusted == false)
+        {
+          order.Price += 1;
+          order.PriceAdjusted = true;
+        }
+        else
+        {
+          order.Price += 2;
+          order.PriceAdjusted = true;
+        }
+      }
     }
     public static void ClearAll()
     {
